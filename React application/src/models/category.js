2098007c8f17d39dc596333752b68b9b09ec8430
Drăghici Andreea-Maria <72825756+AndreeaDraghici@users.noreply.github.com>
2021-06-18 11:58:50 +0300

    Add files via upload

---
+++ React application/src/models/category.js
@@ -0,0 +1,9 @@
+class Category {
+  constructor(categoryId, name, description) {
+    this.categoryId = categoryId;
+    this.name = name;
+    this.description = description;
+  }
+}
+
+export default Category;
---
+++ React application/src/models/product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(productId, name, description, price, basePrice, categoryId, imageName) {
+      this.productId = productId;
+      this.name = name;
+      this.description = description;
+      this.price = price;
+      this.basePrice = basePrice;
+      this.categoryId = categoryId;
+      this.imageName = imageName;
+    }
+  }
+  
+  export default Product;
+  
