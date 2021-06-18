103400d230052122f446e7b4d118e5e220b357ed
Drăghici Andreea-Maria <72825756+AndreeaDraghici@users.noreply.github.com>
2021-06-18 11:52:16 +0300

    Add files via upload

---
+++ API application/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApplication2.Domain;
+
+namespace WebApplication2.Business
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllCategories();
+        Task<Category> GetCategoryByID(int categoryID);
+        Task<Category> InsertCategory(Category category);
+        Task<Category> UpdateCategory(Category category);
+        Task<Category> DeleteCategory(int categoryID);
+        
+    }
+}
---
+++ API application/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApplication2.Domain;
+
+namespace WebApplication2.Business
+{
+    public interface IProductRepository
+    {
+
+        Task<List<Product>> GetAllProducts();
+        Task<Product> GetProductByID(int productID);
+        Task<List<Product>> GetProductByCategoryID(int categoryID);
+        Task<Product> InsertProduct(Product product);
+        Task<Product> UpdateProduct(Product product);
+        Task<Product> DeleteProduct(int productID);
+
+    }
+}
