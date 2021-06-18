e2e009e8fc441e6ab0e852cfbed6e3b34a10d046
Drăghici Andreea-Maria <72825756+AndreeaDraghici@users.noreply.github.com>
2021-06-18 11:52:00 +0300

    Add files via upload

---
+++ API application/Domain/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Domain
+{
+    public class Category
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int CategoryID { get; set; }
+        public virtual string Name { get; set; }
+        [ForeignKey("CategoryID")]
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual string Description { get; set; }
+
+    }
+}
---
+++ API application/Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Domain
+{
+    public class Product
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int ProductID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual decimal Price { get; set; }
+        public virtual decimal BasePrice { get; set; }
+        public virtual int CategoryID { get; set; }
+        public virtual string ImageName { get; set; }
+
+    }
+}
